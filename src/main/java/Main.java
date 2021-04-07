41bf56c455d951bccef60fe3d5300f196ebc90a8
Kuzmichou Aliaksei <kuzmichoualiaksei@gmail.com>
2021-04-07 17:23:28 +0300

    Master-final commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,5 +3,6 @@ public class Main {
         System.out.println("Hello");
         System.out.println("Hello 2");
         System.out.println("cherry-pick");
+        System.out.println("Master final");
     }
 }
